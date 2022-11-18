0974656708bddd0b07062d5605d08f54c09b2b21
joaomgper <joaomgper94@gmail.com>
2022-11-18 15:14:36 +0000

    Setup for v0.0.2

--- setup.bash
+++ setup.bash
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
-GITWS_SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-source ${GITWS_SCRIPT_DIR}/src/gitws.bash
+if [ ! -d ~/.config/gitws ]; then
+    mkdir -p ~/.config/gitws
+fi
+
+wget -O ~/.config/gitws/gitws.bash https://gitlab.com/johnmperg/gitws/-/raw/v0.0.2/src/gitws.bash
+
+if [ ! -z "$(cat ~/.bashrc | grep 'source ~/.config/gitws/gitws.bash')" ]; then
+    source ~/.config/gitws/gitws.bash
+fi
