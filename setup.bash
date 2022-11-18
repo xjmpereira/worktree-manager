8096cb712f80f2a60156fd24575f48ee30f99eb8
joaomgper <joaomgper94@gmail.com>
2022-11-18 15:21:11 +0000

    Fix setup and prepare v0.0.3

--- setup.bash
+++ setup.bash
@@ -4,8 +4,9 @@ if [ ! -d ~/.config/gitws ]; then
     mkdir -p ~/.config/gitws
 fi
 
-wget -O ~/.config/gitws/gitws.bash https://gitlab.com/johnmperg/gitws/-/raw/v0.0.2/src/gitws.bash
+wget -O ~/.config/gitws/gitws.bash https://gitlab.com/johnmperg/gitws/-/raw/v0.0.3/src/gitws.bash
 
 if [ ! -z "$(cat ~/.bashrc | grep 'source ~/.config/gitws/gitws.bash')" ]; then
-    source ~/.config/gitws/gitws.bash
+    echo 'source ~/.config/gitws/gitws.bash' >> ~/.bashrc
 fi
+source ~/.config/gitws/gitws.bash
