648a69eb873adfedaf1525ae69833aff5fbbc06d
joaomgper <joaomgper94@gmail.com>
2025-04-26 14:44:59 +0000

    fix installation

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 GITLAB_NAME=ws
 GITLAB_PROJECT=41089011
 GITWS_BINARY=ws-cmd
-GITWS_VERSION=master
+GITWS_VERSION=main
 GITWS_CONFIG_DIR=.config/gitws
 
 if [ ! -d ~/${GITWS_CONFIG_DIR} ]; then
@@ -10,7 +10,7 @@ if [ ! -d ~/${GITWS_CONFIG_DIR} ]; then
 fi
 
 echo " : Downloading ws shell script"
-wget -qO ~/${GITWS_CONFIG_DIR}/ws.sh https://gitlab.com/johnmperg/gitws/-/raw/master/ws.sh
+wget -qO ~/${GITWS_CONFIG_DIR}/ws.sh https://gitlab.com/johnmperg/gitws/-/raw/main/ws.sh
 
 echo " : Downloading ws binary"
 wget -qO ~/${GITWS_CONFIG_DIR}/${GITWS_BINARY} https://gitlab.com/api/v4/projects/$GITLAB_PROJECT/packages/generic/$GITLAB_NAME/$GITWS_VERSION/$GITWS_BINARY
